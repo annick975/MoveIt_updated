d12667886562f64492a8bb548a458f58dc612fd1
annick975 <niyubahwe.annick975@gmail.com>
2025-03-06 20:16:05 +0200

    Final touches

--- src/app/Home/HomeSections/Testimonials.tsx
+++ src/app/Home/HomeSections/Testimonials.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Quote } from "lucide-react";
 import { motion } from "framer-motion";
-import Image from "next/image";
+
 
 interface Testimonial {
   id: number;
